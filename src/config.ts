ea86f968d4d398cc9a197f239a858fae0fba28ea
Jamyye <33551349+Jamyye@users.noreply.github.com>
2018-12-08 14:20:48 -0600

    changes for aeon

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,37 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
 	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://www.masariwallet.com/api/',
-	mainnetExplorerUrl: "https://msrchain.net/",
-	mainnetExplorerUrlHash: "https://msrchain.net/tx/{ID}",
-	mainnetExplorerUrlBlock: "https://msrchain.net/block/{ID}",
-	testnetExplorerUrl: "http://testnet.msrchain.net/",
-	testnetExplorerUrlHash: "http://testnet.msrchain.net/tx/{ID}",
-	testnetExplorerUrlBlock: "http://testnet.msrchain.net/block/{ID}",
+	mainnetExplorerUrl: "https://aeonblocks.com/",
+	mainnetExplorerUrlHash: "https://aeonblocks.com/tx/{ID}",
+	mainnetExplorerUrlBlock: "https://aeonblocks.com/block/{ID}",
+	testnetExplorerUrl: "http://162.210.173.151/",
+	testnetExplorerUrlHash: "http://162.210.173.151/tx/{ID}",
+	testnetExplorerUrlBlock: "http://162.210.173.151/block/{ID}",
 	testnet: false,
 	coinUnitPlaces: 12,
 	txMinConfirms: 10,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
 	txCoinbaseMinConfirms: 60, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-	addressPrefix: 28,
-	integratedAddressPrefix: 29,
-	addressPrefixTestnet: 33,
-	integratedAddressPrefixTestnet: 34,
-	subAddressPrefix: 52,
-	subAddressPrefixTestnet: 73,
+	addressPrefix: 0xB2,
+	integratedAddressPrefix: 0x2733,
+	addressPrefixTestnet: 0x0426,
+	integratedAddressPrefixTestnet: 0x2C27,
+	subAddressPrefix: 0x06B8,
+	subAddressPrefixTestnet: 0x0AAC,
 	feePerKB: new JSBigInt('400000000'),//20^10 - for testnet its not used, as fee is dynamic.
 	dustThreshold: new JSBigInt('1000000000'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-	defaultMixin: 12, // default value mixin
+	defaultMixin: 5, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'MSR',
+	coinSymbol: 'AEON',
 	openAliasPrefix: "msr",
 	coinName: 'Masari',
 	coinUriPrefix: 'masari:',
-	avgBlockTime: 120,
+	avgBlockTime: 240,
 	maxBlockNumber: 500000000,
 
 	donationAddresses : [
-		'5qfrSvgYutM1aarmQ1px4aDiY9Da7CLKKDo3UkPuUnQ7bT7tr7i4spuLaiZwXG1dFQbkCinRUNeUNLoNh342sVaqTaWqvt8',
-		'5nYWvcvNThsLaMmrsfpRLBRou1RuGtLabUwYH7v6b88bem2J4aUwsoF33FbJuqMDgQjpDRTSpLCZu3dXpqXicE2uSWS4LUP',
-		'9ppu34ocgmeZiv4nS2FyQTFLL5wBFQZkhAfph7wGcnFkc8fkCgTJqxnXuBkaw1v2BrUW7iMwKoQy2HXRXzDkRE76Cz7WXkD'
+		'WmsgtfDBKveDSmsncEion5EMApPdTt4PZWkD2thtWmAmjVSmYaepznV4woX86FwjoYVgzkNiJULpeFHTPB1PTfTr3B4ZVG7ym'
 	]
 };
